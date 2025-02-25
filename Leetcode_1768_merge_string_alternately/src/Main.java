820bda87ffb9b71b9c61b31d334ea6b65c755b85
Rajan <rajan0913@yahoo.com>
2025-02-26 00:16:04 +0530

    Added Solution

--- Leetcode_1768_merge_string_alternately/src/Main.java
+++ Leetcode_1768_merge_string_alternately/src/Main.java
@@ -2,6 +2,8 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-
+        Solution sln = new Solution();
+        String result = sln.mergeAlternately("gtu","ihb");
+        System.out.println(result);
     }
 }
