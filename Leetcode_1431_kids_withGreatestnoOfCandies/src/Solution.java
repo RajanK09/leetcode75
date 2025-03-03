aed2dc7ba5f496d11642e2bb7340459fc5567f19
Rajan <rajan0913@yahoo.com>
2025-03-03 23:19:19 +0530

    optimize to 0 ms for execution

--- Leetcode_1431_kids_withGreatestnoOfCandies/src/Solution.java
+++ Leetcode_1431_kids_withGreatestnoOfCandies/src/Solution.java
@@ -4,12 +4,23 @@ import java.util.List;
 public class Solution {
     public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
 
-        List<Boolean> retdata = new ArrayList<>();
-        int maxCandy=extraCandies;
-        for (int candy:candies){
-            if(maxCandy<=(extraCandies+candy)){
+
+        int max = candies[0];
+        for (int i = 1; i < candies.length;i++){
+            if(candies[i]>max){
+                max=candies[i];
+            }
+        }
+        List<Boolean> retdata = new ArrayList<>(candies.length);
+        if(extraCandies+candies[0]>=max)
+            retdata.add(true);
+        else
+            retdata.add(false);
+
+
+        for (int i = 1; i < candies.length;i++){
+            if(extraCandies+candies[i] >= max){
                 retdata.add(true);
-                maxCandy = candy;
             }
             else
                 retdata.add(false);
